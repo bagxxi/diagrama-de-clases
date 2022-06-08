b3177dbaadf79b3bd7e01d28a04e7aaed7c05a0d
Gabriel Balbontin <bagxxi@gmail.com>
2022-06-07 21:47:37 -0400

    initial commit
    all files uploaded to repository

---
+++ listado_respuestas.py
@@ -0,0 +1,16 @@
+from typing import List
+from usuario import Usuario
+
+class ListadoRespuestas():
+    def __init__(self, usuario: Usuario, respuestas: list) -> None:
+       self.__usuario = Usuario
+       self.__respuestas = respuestas
+
+       @property
+       def usuario(self) -> Usuario:
+           return self.__usuario
+
+       @property
+       def respuestas(self) -> List[Respuesta]:
+           return self.__respuestas
+
---
+++ pregunta.py
@@ -0,0 +1,20 @@
+from alternativa import Alternativa
+
+class Pregunta():
+    def __init__(self, enunciado: str, ayuda: str, alternativas: list, requerido) -> None:
+        self.enunciado = enunciado
+        self.ayuda = ayuda
+        self.__alternativas = [
+            Alternativa(a["contenido"], a["ayuda"]) for a in alternativas]
+        self.requerido = requerido
+
+    def mostrar_pregunta(self) -> None:
+        print(self.enunciado)
+
+        if (self.ayuda):
+            print(f"({self.ayuda})")
+
+        for a in self.__alternativas:
+            a.mostrar_alternativa()
+
+            
